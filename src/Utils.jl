784788f4a2b63a960ad0cdb29dcc6d57c5355764
Aidan Bailey <aidan.j.bailey@outlook.com>
2022-04-02 16:54:25 +0200

    Add useful toString for info

--- src/Utils.jl
+++ src/Utils.jl
@@ -22,19 +22,17 @@ function trainModel!(model::Chain, dataloader::DataLoader, α::Number, epochs::I
     testmode!(model, false)
     reset!(model)
 
-    if epochs < 1
-        error("Number of epochs, $epochs, is less than 1.")
-    end
+    epochs > 0 || error("Number of epochs, $epochs, must be greater than zero.")
 
     @info "Training (α=$α, epochs=$epochs, loss=$loss, optimiser=$optimiser)"
     opt = optimiser(α)
 
-    p = Progress(epochs; dt=1, desc="Training:", barglyphs=BarGlyphs("[=> ]"), showspeed=true)
+    p = Progress(epochs; dt = 1, desc = "Training:", barglyphs = BarGlyphs("[=> ]"), showspeed = true)
     local trainingloss
     for epoch = 1:epochs
         #@info "Epoch: $(epoch)"
         trainingloss = customTrain!(loss, model, dataloader, opt)
-        ProgressMeter.next!(p; showvalues = [(:Epoch,epoch), (:Loss, trainingloss)])
+        ProgressMeter.next!(p; showvalues = [(:Epoch, epoch), (:Loss, trainingloss)])
     end
     return trainingloss
 
@@ -50,7 +48,25 @@ function testModel(model::Chain, dataloader::DataLoader, loss)::Float64
         testloss = loss(ŷ, y)
         avgloss += testloss
     end
-    return avgloss/length(dataloader)
+    return avgloss / length(dataloader)
+end
+
+function toString(heading::String; architecture = nothing, gpu = nothing, optimiser = nothing,
+    loss = nothing, learningrate = nothing, batchsize = nothing, epochs = nothing,
+    trainingsetsize = nothing, testsetsize = nothing, model = nothing)
+    stringList::Vector{String} = [heading]
+    processvar(name, var) = isnothing(var) || push!(stringList, "$name: $var")
+    processvar("Architecture", architecture)
+    processvar("Model", model)
+    processvar("GPU", gpu)
+    processvar("Optimiser", optimiser)
+    processvar("Loss", loss)
+    processvar("Learning rate", learningrate)
+    processvar("Batch size", batchsize)
+    processvar("Epochs", epochs)
+    processvar("Train set size", trainingsetsize)
+    processvar("Test set size", testsetsize)
+    return join(stringList, "\n")
 end
 
 end
